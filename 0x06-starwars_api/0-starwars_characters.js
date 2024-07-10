b79036761c0c084c91f147c8568b60abc9244573
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-10 09:59:23 +0100

    Star wars characters API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieId + '/';
-
-request(url, (error, response, body) => {
-  if (error) {
-    throw error;
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        throw error;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
