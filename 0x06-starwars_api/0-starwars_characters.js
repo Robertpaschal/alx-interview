a30ca7463c3526d65b13cb70153bb5fb93b93ef0
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-07-10 09:41:34 +0100

    Star wars characters API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,11 +3,6 @@
 const request = require('request');
 const movieId = process.argv[2];
 
-if (!movieId) {
-  console.error('Please provide a Movie ID as the first argument');
-  process.exit(1);
-}
-
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId + '/';
 
 request(url, (error, response, body) => {
