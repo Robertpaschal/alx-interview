35076dd4af5c249af5f164cdd163d2da0fef63eb
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-08-07 21:32:31 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,57 +2,31 @@
 """ Module containing the game-winner function"""
 
 
+# 0-prime_game.py
+
 def isWinner(x, nums):
-    """Function defining constraints for the game-winner"""
-    def sieve(n):
-        """Return a list of primes up to n using Sieve of Eratosthenes"""
+    def count_primes_up_to(n):
+        """ Returns the number of primes <= n using the Sieve of Eratosthenes. """
+        if n < 2:
+            return 0
         is_prime = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if is_prime[p]]
-
-    def play_game(n):
-        """ Simulate the game for given n
-        and return the winner: 'Maria' or 'Ben' """
-        primes = sieve(n)
-        if not primes:
-            return 'Ben'
-        # Boolean list to keep track of available numbers
-        available = [True] * (n + 1)
-        current_player = 0  # 0 for Maria, 1 for Ben
-
-        while True:
-            move_made = False
-            for prime in primes:
-                if available[prime]:
-                    for multiple in range(prime, n + 1, prime):
-                        available[multiple] = False
-                    move_made = True
-                    break
-
-            if not move_made:
-                return 'Maria' if current_player == 1 else 'Ben'
-
-            current_player = 1 - current_player
-
-    if x <= 0 or not nums:
-        return None
-
+        is_prime[0] = is_prime[1] = False
+        for start in range(2, int(n**0.5) + 1):
+            if is_prime[start]:
+                for multiple in range(start*start, n + 1, start):
+                    is_prime[multiple] = False
+        return sum(is_prime)
+    
     maria_wins = 0
     ben_wins = 0
-
+    
     for n in nums:
-        winner = play_game(n)
-        print(f"Game with n={n} won by {winner}")  # Debug print
-        if winner == 'Maria':
+        prime_count = count_primes_up_to(n)
+        if prime_count % 2 == 1:
             maria_wins += 1
-        elif winner == 'Ben':
+        else:
             ben_wins += 1
-
+    
     if maria_wins > ben_wins:
         return 'Maria'
     elif ben_wins > maria_wins:
