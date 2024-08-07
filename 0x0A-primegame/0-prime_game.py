3325f7527fb99d2fd2d3a2dfed29c8d820cb81f3
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-08-07 17:52:23 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module containing the game-winner function"""
 
+
 def isWinner(x, nums):
     """Function defining contraints for the game-winner"""
     def sieve(n):
@@ -13,15 +14,16 @@ def isWinner(x, nums):
                     is_prime[i] = False
             p += 1
         return [p for p in range(2, n + 1) if is_prime[p]]
-    
+
     def play_game(n):
-        """ Simulate the game for given n and return the winner: 'Maria' or 'Ben """
+        """ Simulate the game for given n
+        and return the winner: 'Maria' or 'Ben """
         primes = sieve(n)
         if not primes:
             return 'Ben'
         # Boolean list to keep track of available numbers
         available = [True] * (n + 1)
-        current_player = 0 # 0 for Maria, 1 for Ben
+        current_player = 0   # 0 for Maria, 1 for Ben
 
         while primes:
             prime = primes[0]
@@ -33,23 +35,24 @@ def isWinner(x, nums):
             primes.pop(0)
 
             # check if thers area any primes left for the next player
-            next_available_primes = [p for p in range(2, n + 1) if available[p] and p in primes]
+            next_available_primes = [p for p in range(
+                2, n + 1) if available[p] and p in primes]
             if not next_available_primes:
                 return 'Maria' if current_player == 1 else 'Ben'
-            
+
             # Switch player
             current_player = 1 - current_player
         return 'Maria' if current_player == 1 else 'Ben'
     if x <= 0 or not nums:
         return None
-    
+
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
         winner = play_game(n)
         if winner == 'Maria':
-            maria_wins +=1
+            maria_wins += 1
         elif winner == 'Ben':
             ben_wins += 1
 
